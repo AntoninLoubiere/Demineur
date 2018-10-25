d5eaacfd480dc923c1dc6862cac949a2b33cfe8d
AntoninLoubiere <antonin.loubiere@laposte.net>
2018-10-25 11:19:02 +0200

    Add game class

---
+++ src/Game/Game.cpp
@@ -0,0 +1,21 @@
+/*
+ * Game.cpp
+ *
+ *  Created on: 25 oct. 2018
+ *      Author: antonin
+ */
+
+#include "Game.h"
+
+namespace ta {
+
+Game::Game() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Game::~Game() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace ta */
---
+++ src/Game/Game.h
@@ -0,0 +1,21 @@
+/*
+ * Game.h
+ *
+ *  Created on: 25 oct. 2018
+ *      Author: antonin
+ */
+
+#ifndef SRC_GAME_GAME_H_
+#define SRC_GAME_GAME_H_
+
+namespace ta {
+
+class Game {
+public:
+	Game();
+	virtual ~Game();
+};
+
+} /* namespace ta */
+
+#endif /* SRC_GAME_GAME_H_ */
